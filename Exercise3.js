2704c7501ca22461b24d92f46065ec709ef0c992
Hope-Avritch <hopema66@gmail.com>
2023-08-29 15:26:51 -0400

    ex.3 primitive data type, string

---
+++ Exercise3.js
@@ -0,0 +1,10 @@
+//Ex.3
+console.log("This is a string, it has quotes.");
+console.log(100);
+console.log("100");
+console.log("My name is: Hope");
+console.log(); //change this line
+console.log("My age is: 37");
+console.log(); //change this line, which type should you use? String
+console.log("My zip code is: 06062"); 
+console.log(); //change this line, which type should you use? String
