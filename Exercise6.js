a63339cedca6e0ed3326f686e597aa87f2dd651f
Hope-Avritch <hopema66@gmail.com>
2023-08-29 16:27:58 -0400

    Ex. 6 Comparison with strings

---
+++ Exercise6.js
@@ -0,0 +1,23 @@
+console.log("true or false: wrapping in double quotes is the same as wrapping in single quotes"); True
+console.log("Hello world!" == 'Hello world!');
+
+console.log("true or false: using single quotes inside a string is the same as using double quotes inside the string"); False
+console.log("He said 'no'!" == 'He said "no"!');
+
+console.log("true or false: strings that say the same thing but have different capitalization are not equal"); True
+console.log("Hello world!" != "hello world!");
+
+console.log("true or false: in javascript, a string is considered less than another string if it would come before it in alphabetical order"); True
+console.log("apple" < "banana");
+
+console.log("The number 5 is the same value as the string '5'"); True
+console.log(5 == "5");
+
+console.log("The number 5 is the same value and same data type as the string '5'"); false
+console.log(5 === "5"); 
+
+console.log("Mom" == 'Mom'); // ==, true
+console.log(carrot < blanket); // <, false
+console.log("Jurassic Park" !== '"jurassic" park'); // !==, false
+console.log(6 != 7); // !=, true
+console.log('6' === "6"); // ===, true
