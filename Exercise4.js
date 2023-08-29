13a109a3584d3000415c71a9abfb15eac285c064
Hope-Avritch <hopema66@gmail.com>
2023-08-29 15:43:56 -0400

    Ex 4 Arithmetic Operators

---
+++ Exercise4.js
@@ -0,0 +1,39 @@
+console.log("5 + 4 = ");
+console.log(5 + 4);
+
+console.log("10 - 7 = ");
+console.log(10 - 7);
+
+console.log("6 * 3 = ");
+console.log(6 * 3);
+
+console.log("18 / 2 = ");
+console.log(18 / 2);
+
+console.log("4 to the 2nd power is: ");
+console.log(4 ** 2);
+
+console.log("The remainder when 25 is divided by 4 is: ");
+console.log(25 % 4);
+
+console.log("13 + 8 / 2 - 1 = ");
+console.log(13 + 8 / 2 - 1); //What will this equal? Remember Order of Operations!  18
+
+console.log("(13 + 8) / (2 - 1) = ");
+console.log((13 + 8) / (2 - 1)); //What will this equal? Is it the same as the previous line? 21 NO
+
+console.log("The sum of 9 and 15 is:23 ");
+console.log(); //change this line
+console.log("The difference between 100 and 48 is:52");
+console.log(); //change this line
+console.log("The product of 56 and 32 is:1792");
+console.log(); //change this line
+console.log("The quotient of 198 and 6 is:33");
+console.log(); //change this line
+console.log("3 raised to the 5th power is:3**5 ");
+console.log(); //change this line
+console.log("The remainder when 45 is divided by 7 is: 45 % 7 ")
+console.log(); //change this line
+console.log("The product of 5 and 7 added to the quotient of 12 and 3 is:39 ");
+console.log(); //change this line
+
