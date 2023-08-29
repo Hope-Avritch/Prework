dcaef8dbf0b231cb3a43825f769791c547920465
Hope-Avritch <hopema66@gmail.com>
2023-08-29 16:04:24 -0400

    Ex 5 Comparison Operations & Booleans

---
+++ Exercise5.js
@@ -0,0 +1,11 @@
+console.log("True or false: 5 is less than 2");
+console.log(5 < 2);
+console.log("True or false: 19 is not equal 11");
+console.log(19 != 11)
+console.log("True or false: 10 is greater than or equal to 10");
+console.log(10 >= 10);
+
+console.log(10 == 2 + 8 ); // ==, true
+console.log(3 > 6); // >, false
+console.log(10 + 4 <= 12); // <=, false
+console.log(4 + 3 != 2 + 6 ); // !=, true
