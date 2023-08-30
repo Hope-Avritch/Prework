638663ca8c41887e2a145a6c9a69cdd132e00204
Hope-Avritch <hopema66@gmail.com>
2023-08-29 21:05:59 -0400

    ex 5 and 7 updates

--- Exercise5.js
+++ Exercise5.js
@@ -1,9 +1,9 @@
 //Exercise 5
-console.log("True or false: 5 is less than 2"); False
+console.log("True or false: 5 is less than 2"); 
 console.log(5 < 2);
-console.log("True or false: 19 is not equal 11"); True
+console.log("True or false: 19 is not equal 11"); 
 console.log(19 != 11)
-console.log("True or false: 10 is greater than or equal to 10"); True
+console.log("True or false: 10 is greater than or equal to 10"); 
 console.log(10 >= 10);
 
 console.log(10 == 2 + 8 ); // ==, true
--- Exercise7.js
+++ Exercise7.js
@@ -26,16 +26,17 @@ const juansDrinks = 18.00
 const sheasMeal = 21.99
 const sheasDrinks = 10.00
 
-const mealsTotal = mikesMeal + juansMeal + sheasMeal //complete the line to calculate the total price for all of the meals
-const drinksTotal = mikesDrinks + juansDrinks + sheasDrinks//complete the line to calculate the total price for all of the meals
+const mealsTotal = mikesMeal + juansMeal + sheasMeal;//complete the line to calculate the total price for all of the meals
+const drinksTotal = mikesDrinks + juansDrinks + sheasDrinks;//complete the line to calculate the total price for all of the meals
 
-const subtotal = mealsTotal + drinksTotal//calculate the subtotal for the entire bill (both drinks and meals)
+const subtotal = mealsTotal + drinksTotal;//calculate the subtotal for the entire bill (both drinks and meals)
 
-const tax = (subtotal * .10) //calculate the tax on the bill assuming a 10% tax rate
-const tip = tax + subtotal  * .20 //calculate what a 20% tip on the bill would be
+const tax = (subtotal * .10); //calculate the tax on the bill assuming a 10% tax rate
+const tip = subtotal  * .20; //calculate what a 20% tip on the bill would be
 
-const billTotal = tip + subtotal //calculate the total due for the bill
-const splitAmount = billTotal / 3//calculate how much each person pays if they decide to split the bill evenly
+const billTotal = tax + tip + subtotal; //calculate the total due for the bill
+const splitAmount = billTotal / 3;//calculate how much each person pays if they decide to split the bill evenly
 
 console.log("The total due is", billTotal.toFixed(2));
 console.log("Split 3 ways is", splitAmount.toFixed(2), "each");
+//to fixed(2) = 2 decimal places
