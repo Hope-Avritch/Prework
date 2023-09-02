aff37280265b3e54c07aa7cd68475a53c52cd41b
Hope-Avritch <hopema66@gmail.com>
2023-09-02 17:08:52 -0400

    dayfour exercises

--- Exercise8.1.js
+++ Exercise8.1.js
@@ -1,8 +1,8 @@
 //Ex. 8.1
-const firstName ="Hope";//add your first name
-const lastName ="Avritch";//add your last name
-const fullNameConcat = "firstName" + " " + "lastName";//create your full name from the first two variables using concatenation 
-const fullNameInterp = `${firstName} + ${lastName}`; //create your full name from the first two variables using interpolation
+const firstName = 'Hope';//add your first name
+const lastName = 'Avritch';//add your last name
+const fullNameConcat = "firstName" + "lastName";//create your full name from the first two variables using concatenation 
+const fullNameInterp = '${firstName} ${lastName}'; //create your full name from the first two variables using interpolation
 
 //don't change the following line, checks to see that concatenation and interpolation gave results
 console.log("Are your fullNameConcat and fullNameInterp variables equal?")
--- Exercise8.js
+++ Exercise8.js
@@ -1,10 +1,31 @@
 //Ex. 8.1
-const firstName ="Hope";//add your first name
-const lastName ="Avritch";//add your last name
-const fullNameConcat = "firstName" + " " + "lastName";//create your full name from the first two variables using concatenation 
+//const firstName = "Jane";
+//const lastName = "Austen";
+//const fullName = firstName + lastName;
+//const fullName = firstName + " " + lastName;
+
+//console.log("The full name is", fullName);
+
+//const patientName = "Skyler Rose"//"Evelyn Park";
+//let apptTime = "1600"//"2:30pm";
+//let apptDay = "Sartuday"//"Thursday";
+
+//let reminderMessage = `Hi ${patientName}! This is a reminder that you have an appointment at ${apptTime} on ${apptDay}. See you then!`;
+
+//console.log(reminderMessage);
+
+const firstName = "Hope"//add your first name
+const lastName = "Avritch"//add your last name
+const fullNameConcat = "firstName" + "lastName";//create your full name from the first two variables using concatenation 
 const fullNameInterp = `${firstName} + ${lastName}`; //create your full name from the first two variables using interpolation
 
 //don't change the following line, checks to see that concatenation and interpolation gave results
 console.log("Are your fullNameConcat and fullNameInterp variables equal?")
 console.log(fullNameConcat == fullNameInterp); 
 
+//let myAge = 37//add your age
+//let faveFood = lobster//add your favorite food
+//let faveColor = green//add your favorite color
+//let miniBio = `${myAge} +  ${faveFood} + ${faveColor}`; //use string interpolation to create summary that includes your full name, age, favorite food, and favorite color
+//console.log("Your mini bio is:");
+//console.log(miniBio);
