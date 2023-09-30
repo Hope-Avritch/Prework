da9cb61af36e3d174a28e00b7d24c1ee70c8f1c8
Hope-Avritch <hopema66@gmail.com>
2023-09-30 12:56:30 -0400

    updated work

--- Exercise8.js
+++ Exercise8.js
@@ -14,18 +14,18 @@
 
 //console.log(reminderMessage);
 
-const firstName = "Hope"//add your first name
-const lastName = "Avritch"//add your last name
-const fullNameConcat = "firstName" + "lastName";//create your full name from the first two variables using concatenation 
+const firstName = "Hope";//add your first name
+const lastName = "Avritch";//add your last name
+const fullNameConcat = firstName + lastName;//create your full name from the first two variables using concatenation 
 const fullNameInterp = `${firstName} + ${lastName}`; //create your full name from the first two variables using interpolation
 
 //don't change the following line, checks to see that concatenation and interpolation gave results
 console.log("Are your fullNameConcat and fullNameInterp variables equal?")
 console.log(fullNameConcat == fullNameInterp); 
 
-//let myAge = 37//add your age
-//let faveFood = lobster//add your favorite food
-//let faveColor = green//add your favorite color
-//let miniBio = `${myAge} +  ${faveFood} + ${faveColor}`; //use string interpolation to create summary that includes your full name, age, favorite food, and favorite color
-//console.log("Your mini bio is:");
-//console.log(miniBio);
+let myAge = "37";//add your age
+let faveFood = "lobster";//add your favorite food
+let faveColor = "green";//add your favorite color
+let miniBio = `Hi. I am ${myAge} years old.  My favorite food is ${faveFood}.  My favorite color is ${faveColor}.`; //use string interpolation to create summary that includes your full name, age, favorite food, and favorite color
+console.log("Your mini bio is:");
+console.log(miniBio);
